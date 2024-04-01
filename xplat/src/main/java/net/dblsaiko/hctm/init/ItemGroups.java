4a1f34b84e754f7af873ae9e096d96497a5a90a6
Kneelawk <kneelawk@gmail.com>
2024-04-01 01:57:27 -0700

    Fix item group builder

--- xplat/src/main/java/net/dblsaiko/hctm/init/ItemGroups.java
+++ xplat/src/main/java/net/dblsaiko/hctm/init/ItemGroups.java
@@ -3,8 +3,6 @@ package net.dblsaiko.hctm.init;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.text.Text;
 
-import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroup;
-
 import net.dblsaiko.hctm.HctmBase;
 
 import static net.dblsaiko.hctm.HctmBase.MOD_ID;
@@ -15,7 +13,7 @@ public class ItemGroups {
     public final RegistryObject<ItemGroup> all;
 
     public ItemGroups(Items items) {
-        all = reg.create("all", ItemGroup.builder()
+        all = reg.create("all", ItemGroup.create(ItemGroup.Row.TOP, 0)
             .displayName(Text.translatable("itemGroup.hctm-base.all"))
             .icon(() -> HctmBase.getInstance().items.getScrewdriver().getDefaultStack())
             .entries((displayContext, entries) -> {
